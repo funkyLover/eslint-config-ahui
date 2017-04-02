795d06d16a78cad08cc32752a30f95c8e275fbcc
林龙辉 <540886643@qq.com>
2017-04-02 21:13:50 +0800

    * vue-eslint

--- vue-eslint.js
+++ vue-eslint.js
@@ -1,5 +1,20 @@
 module.exports = {
   // for Vuejs
   root: true,
-  extends: './index.js'
+  extends: './index.js',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true
+  },
+  plugins: [
+    'html'
+  ],
+  rules: {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0
+  }
 }
